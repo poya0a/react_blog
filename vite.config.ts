7510c42f95864e17b43d3d62faa00159cfbb6ebb
unknown <poya0a@cnspartner.com>
2025-01-03 17:38:49 +0900

    first commmit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({jsxRuntime: 'classic'}), viteTsconfigPaths(), svgrPlugin()],
 })
